d0d18d4b84b65df517bda505802d6fa0fcf37712
JoshLDowns <josh.lloyd.downs@gmail.com>
2020-07-22 09:03:09 -0400

    removed unused vars

--- src/components/OrderInStock.js
+++ src/components/OrderInStock.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 import OrderInStockTableView from "./OrderInStockTableView";
 import OrderPreGridView from "./OrderInStockgridView";
@@ -6,42 +6,23 @@ import OrderPreGridView from "./OrderInStockgridView";
 //mockdata
 import items from "../assets/mockdata/Items";
 
-import { makeStyles } from "@material-ui/core/styles";
-
-//import StarIcon from "@material-ui/icons/Star";
-
-const useStyles = makeStyles((theme) => ({
-  ...theme.global,
-  queryForms: {
-    float: "right",
-  },
-  formControl: {
-    width: "150px",
-    margin: "0 5px",
-  },
-  configButtons: {
-    marginLeft: "70%",
-  },
-}));
-
 const OrderInStock = (props) => {
-  const classes = useStyles();
 
   const { currentView, handlePreview } = props;
 
-  const [currentItems, setCurrentItems] = useState(items);
+  //const [currentItems, setCurrentItems] = useState(items);
 
   return (
     <>
       {currentView === "list" && (
         <OrderInStockTableView
-          currentItems={currentItems}
+          currentItems={items}
           handlePreview={handlePreview}
         />
       )}
       {currentView === "grid" && (
         <OrderPreGridView
-          currentItems={currentItems}
+          currentItems={items}
           handlePreview={handlePreview}
         />
       )}
