6a3c32f4f8a1969fbcea94fe758b36c7ebdc205b
Josh Downs <jdowns@burlingtoncodeacademy.com>
2020-09-01 19:48:37 -0400

    fixed styling error on large format screens

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 
+import "./Index.css"
 import App from "./App";
 import ErrorBoundary from "./components/Utility/ErrorBoundary";
 import store from "./redux/store";
