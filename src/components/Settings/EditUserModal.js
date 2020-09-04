0bff6f1d428025bce9e9aa6ee3432b7e74866607
Josh Downs <jdowns@burlingtoncodeacademy.com>
2020-09-04 14:45:18 -0400

    updated the update user modal window to match the new user set up flow

--- src/components/Settings/EditUserModal.js
+++ src/components/Settings/EditUserModal.js
@@ -3,33 +3,22 @@ import PropTypes from "prop-types";
 
 import { useInput } from "../../hooks/UtilityHooks";
 
-import MaterialTable from "material-table";
+import UserRoleSelect from "./UserRoleSelect";
+import UserTerritoryTable from "./UserTerritoryTable";
 
+import Tooltip from "@material-ui/core/Tooltip";
 import AutoComplete from "@material-ui/lab/Autocomplete";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
-import FormLabel from "@material-ui/core/FormLabel";
-import FormControl from "@material-ui/core/FormControl";
-import FormGroup from "@material-ui/core/FormGroup";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
 import IconButton from "@material-ui/core/IconButton";
 import { makeStyles } from "@material-ui/core/styles";
 
+import AddCircleIcon from "@material-ui/icons/AddCircle";
 import CancelIcon from "@material-ui/icons/Cancel";
-import AddIcon from "@material-ui/icons/Add";
-import { tableIcons } from "../../utility/tableIcons";
 
-const regions = [
-  "Region 1",
-  "Region 2",
-  "Region 3",
-  "Region 4",
-  "Key Acct. 1",
-  "Key Acct. 2",
-];
+import { regions, keyAccounts } from "../../utility/constants";
 
 const useStyles = makeStyles((theme) => ({
   ...theme.global,
@@ -38,60 +27,58 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const EditUserModal = ({ handleClose }) => {
+const EditUserModal = ({ handleClose, userId }) => {
   const classes = useStyles();
 
+  const regionArray = regions.map((reg) => reg.name);
   const { value: firstName, bind: bindFirstName } = useInput("");
   const { value: lastName, bind: bindLastName } = useInput("");
   const { value: email, bind: bindEmail } = useInput("");
   const { value: phone, bind: bindPhone } = useInput("");
+  const { value: password, bind: bindPassword } = useInput("");
 
   const [region, setRegion] = useState(null);
-
-  const [roles, setRoles] = useState({
-    fieldOne: false,
-    fieldTwo: false,
-    compliance: false,
-    finance: false,
-    supplier: false,
-    superUser: false,
-  });
+  const [keyAccount, setKeyAccount] = useState(null);
+  const [role, setRole] = useState("field1");
 
   const [regionsList, setRegionsList] = useState([]);
-
-  const handleRoleChange = (evt) => {
-    setRoles({ ...roles, [evt.target.name]: evt.target.checked });
-  };
+  const [keyAccountList, setKeyAccountList] = useState([]);
 
   const handleRegion = () => {
     const currentRegions = [...regionsList];
-    if (currentRegions.filter((reg) => reg.region === region).length === 0) {
-      currentRegions.push({ region: region });
+    if (currentRegions.filter((reg) => reg === region).length === 0) {
+      currentRegions.push(region);
       setRegionsList(currentRegions);
     }
   };
 
-  const {
-    fieldOne,
-    fieldTwo,
-    compliance,
-    finance,
-    supplier,
-    superUser,
-  } = roles;
-  const error =
-    [fieldOne, fieldTwo, compliance, finance, supplier, superUser].filter(
-      (v) => v
-    ).length < 1;
+  const handleKeyAccount = () => {
+    const currentKeyAccounts = [...keyAccountList];
+    if (currentKeyAccounts.filter((acct) => acct === keyAccount).length === 0) {
+      currentKeyAccounts.push(keyAccount);
+      setKeyAccountList(currentKeyAccounts);
+    }
+  };
+
+  const handleRemove = (terr, type) => {
+    if (type === "region") {
+      const newRegions = regionsList.filter((reg) => reg !== terr);
+      setRegionsList(newRegions);
+    } else if (type === "keyAccount") {
+      const newAccounts = keyAccountList.filter((acct) => acct !== terr);
+      setKeyAccountList(newAccounts);
+    }
+  };
 
   const handleFormSubmit = (evt) => {
     evt.preventDefault();
     console.log(evt.target.id);
     //temporarily removing unused vars so build will work on netlify, not permenant
-    console.log(firstName, lastName, email, phone);
+    console.log(firstName, lastName, email, phone, password);
   };
+
   return (
-    <div style={{ overflow: "hidden" }}>
+    <>
       <IconButton
         className={classes.closeButton}
         onClick={() => {
@@ -141,6 +128,17 @@ const EditUserModal = ({ handleClose }) => {
           {...bindPhone}
           fullWidth
         />
+        <TextField
+          className={classes.settingsMargin}
+          variant="outlined"
+          color="secondary"
+          name="password"
+          type="text"
+          label="Password"
+          {...bindPassword}
+          fullWidth
+        />
+        <br />
         <br />
         <Typography
           className={classes.headerText}
@@ -148,72 +146,7 @@ const EditUserModal = ({ handleClose }) => {
         >
           User Role
         </Typography>
-
-        <FormControl required error={error} component="fieldset">
-          <FormLabel component="legend">Pick at least 1</FormLabel>
-          <FormGroup row>
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={fieldOne}
-                  onChange={handleRoleChange}
-                  name="fieldOne"
-                />
-              }
-              label="Field User Lvl. One"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={fieldTwo}
-                  onChange={handleRoleChange}
-                  name="fieldTwo"
-                />
-              }
-              label="Field User Lvl. Two"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={compliance}
-                  onChange={handleRoleChange}
-                  name="compliance"
-                />
-              }
-              label="Compliance"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={finance}
-                  onChange={handleRoleChange}
-                  name="finance"
-                />
-              }
-              label="Finance"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={supplier}
-                  onChange={handleRoleChange}
-                  name="supplier"
-                />
-              }
-              label="Supplier"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={superUser}
-                  onChange={handleRoleChange}
-                  name="superUser"
-                />
-              }
-              label="Super"
-            />
-          </FormGroup>
-        </FormControl>
+        <UserRoleSelect role={role} setRole={setRole} />
       </form>
       <br />
       <br />
@@ -223,68 +156,106 @@ const EditUserModal = ({ handleClose }) => {
       >
         Region / Key Acct. Assignment
       </Typography>
-      <Grid container spacing={2}>
+      <Grid container spacing={6}>
         <Grid item md={6} xs={12}>
-          <div style={{ display: "flex", flexWrap: "none", width: "100%" }}>
+          <div
+            style={{
+              display: "flex",
+              flexWrap: "none",
+              width: "100%",
+              alignItems: "center",
+            }}
+          >
             <AutoComplete
               fullWidth
-              className={classes.settingsMargin}
               value={region}
               onChange={(event, value) => setRegion(value)}
               id="region"
-              options={regions}
+              options={regionArray}
               getOptionLabel={(region) => region}
               renderInput={(params) => (
                 <TextField
                   color="secondary"
                   {...params}
-                  label="Region / Key Acct."
+                  label="Region"
                   variant="outlined"
                   size="small"
                 />
               )}
             />
-            <Button
-              style={{
-                height: "40px",
-                marginLeft: "5px",
-              }}
-              variant="contained"
-              color="primary"
-              id="addRegion"
-              disabled={region === null}
-              onClick={handleRegion}
-            >
-              <AddIcon color="secondary" />
-            </Button>
+            <Tooltip title="Add Region">
+              <span>
+                <IconButton
+                  style={{
+                    marginLeft: "5px",
+                  }}
+                  id="addRegion"
+                  disabled={region === null}
+                  onClick={handleRegion}
+                >
+                  <AddCircleIcon color="secondary" />
+                </IconButton>
+              </span>
+            </Tooltip>
           </div>
+          <br />
+          <UserTerritoryTable
+            type={"region"}
+            territories={regionsList}
+            handleRemove={handleRemove}
+          />
         </Grid>
         <Grid item md={6} xs={12}>
-          <MaterialTable
-            title=""
-            columns={[{ title: "Region/Key Account", field: "region" }]}
-            data={regionsList}
-            editable={{
-              onRowDelete: (oldData) =>
-                new Promise((resolve, reject) => {
-                  setTimeout(() => {
-                    const dataDelete = [...regionsList];
-                    const index = oldData.tableData.id;
-                    dataDelete.splice(index, 1);
-                    setRegionsList([...dataDelete]);
-                    resolve();
-                  }, 500);
-                }),
+          <div
+            style={{
+              display: "flex",
+              flexWrap: "none",
+              width: "100%",
+              alignItems: "center",
             }}
-            icons={tableIcons}
-            options={{
-              paging: false,
-              search: false,
-              toolbar: false,
-            }}
+          >
+            <AutoComplete
+              fullWidth
+              value={keyAccount}
+              onChange={(event, value) => setKeyAccount(value)}
+              id="keyAccount"
+              options={keyAccounts}
+              getOptionLabel={(keyAccount) => keyAccount}
+              renderInput={(params) => (
+                <TextField
+                  color="secondary"
+                  {...params}
+                  label="Key Account"
+                  variant="outlined"
+                  size="small"
+                />
+              )}
+            />
+            <Tooltip title="Add Key Account">
+              <span>
+                <IconButton
+                  style={{
+                    marginLeft: "5px",
+                  }}
+                  id="addRegion"
+                  disabled={keyAccount === null}
+                  onClick={handleKeyAccount}
+                >
+                  <AddCircleIcon color="secondary" />
+                </IconButton>
+              </span>
+            </Tooltip>
+          </div>
+          <br />
+          <UserTerritoryTable
+            type={"keyAccount"}
+            territories={keyAccountList}
+            handleRemove={handleRemove}
           />
         </Grid>
       </Grid>
+      <br />
+      <br />
       <Button
         className={classes.largeButton}
         variant="contained"
@@ -294,7 +265,7 @@ const EditUserModal = ({ handleClose }) => {
       >
         SUBMIT
       </Button>
-    </div>
+    </>
   );
 };
 
