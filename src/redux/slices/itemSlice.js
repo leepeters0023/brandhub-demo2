0abd6be84872aeb18315759d2b880094ca623f25
Josh Downs <jdowns@burlingtoncodeacademy.com>
2020-09-08 14:52:37 -0400

    finished initial item slice

--- src/redux/slices/itemSlice.js
+++ src/redux/slices/itemSlice.js
@@ -2,9 +2,11 @@ import { createSlice } from "@reduxjs/toolkit";
 
 let initialState = {
   isLoading: false,
+  orderType: null,
   itemsPerPage: 20,
   totalPages: null,
-  pagesLoaded: null,
+  pagesLoaded: 0,
+  nextLink: null,
   items: [],
   error: null,
 }
@@ -25,13 +27,29 @@ const itemSlice = createSlice({
   reducers: {
     setIsLoading: startLoading,
     setTotalPages(state, action) {
-
+      const { pageCount } = action.payload;
+      state.totalPages = pageCount
     },
     getItemsSuccess(state, action) {
-
+      const { orderType, items, nextLink } = action.payload;
+      let currentItems = [...state.items]
+      let updatedItems = currentItems.concat(items);
+      state.orderType = orderType;
+      state.pagesLoaded += 1;
+      state.nextLink = nextLink;
+      state.items = updatedItems;
+      state.isLoading = false;
+      state.error = null
     },
     setFailure: loadingFailed
   }
 })
+
+export const {
+  setIsLoading,
+  setTotalPages,
+  getItemsSuccess,
+  setFailure
+} = itemSlice.actions;
 
 export default itemSlice.reducer();
