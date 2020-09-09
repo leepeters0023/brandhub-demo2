7ece1b1a30dbb51ea10cc2d92fbea4421c6c00ad
Josh Downs <jdowns@burlingtoncodeacademy.com>
2020-09-09 17:10:22 -0400

    updated loading state styles

--- src/components/Utility/Loading.js
+++ src/components/Utility/Loading.js
@@ -1,26 +1,33 @@
-import React from 'react'
+import React from "react";
 
 import CircularProgress from "@material-ui/core/CircularProgress";
 
 const Loading = ({ partial }) => {
   return (
     <div
-          style={{
-            width: "100vw",
-            height: "100vh",
-            overflow: "hidden",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            backgroundColor: "white",
-            zIndex: "10000",
-          }}
-        >
-          <br />
-          <CircularProgress color="inherit" disableShrink size={100}/>
+      style={{
+        position: "fixed",
+        top: 0,
+        width: "100vw",
+        height: "100vh",
+        overflow: "hidden",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: partial ? "rgb(0,0,0,0)" : "white",
+        zIndex: "10000",
+      }}
+    >
+      <br />
+
+      <CircularProgress
+        color="secondary"
+        disableShrink
+        size={100}
+        />
         </div>
-  )
-}
+  );
+};
 
 export default Loading;
