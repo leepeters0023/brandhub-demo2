3288d76c6141462acd15c9dddfcf63a7d84eef90
Josh Downs <jdowns@burlingtoncodeacademy.com>
2020-09-14 14:48:59 -0400

    more error handling fixes

--- src/redux/slices/patchOrderSlice.js
+++ src/redux/slices/patchOrderSlice.js
@@ -1,6 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-import { patchOrderItem, deletePreOrderItem, submitPreOrder } from "../../api/orderApi";
+import {
+  patchOrderItem,
+  deletePreOrderItem,
+  submitPreOrder,
+} from "../../api/orderApi";
 
 import { setPreOrderProgramStatus } from "../../api/programApi";
 
@@ -22,7 +26,7 @@ let initialState = {
   cellsLoading: [],
   cellsError: [],
   error: null,
-}
+};
 
 const startLoading = (state) => {
   state.isLoading = true;
@@ -30,11 +34,15 @@ const startLoading = (state) => {
 
 const loadingFailed = (state, action) => {
   const { error, cell } = action.payload;
-  let currentErrors = [...state.cellsError];
-  currentErrors.push(cell);
-  state.cellsError = currentErrors;
-  state.isLoading = false;
-  state.error = error;
+  if (cell) {
+    let currentErrors = [...state.cellsError];
+    currentErrors.push(cell);
+    state.cellsError = currentErrors;
+    state.isLoading = false;
+  } else {
+    state.isLoading = false;
+    state.error = error;
+  }
 };
 
 const patchOrderSlice = createSlice({
@@ -45,27 +53,32 @@ const patchOrderSlice = createSlice({
     addLoadingCell(state, action) {
       const { cell } = action.payload;
       let currentLoading = [...state.cellsLoading];
-      currentLoading.push(cell)
+      currentLoading.push(cell);
       state.cellsLoading = currentLoading;
     },
     patchItemSuccess(state, action) {
-      const {id, orderNumber} = action.payload;
+      const { id, orderNumber } = action.payload;
       let currentLoading = [...state.cellsLoading];
-      let currentCell = currentLoading.find((cell) => cell.id === id && cell.orderNumber === orderNumber);
-      currentLoading.splice(currentLoading.indexOf(currentCell), 1)
+      let currentCell = currentLoading.find(
+        (cell) => cell.id === id && cell.orderNumber === orderNumber
+      );
+      currentLoading.splice(currentLoading.indexOf(currentCell), 1);
 
-      state.cellsLoading = [...currentLoading]
-      state.isLoading = false
+      state.cellsLoading = [...currentLoading];
+      state.isLoading = false;
+      state.error = null;
     },
     setProgStatusSuccess(state) {
       state.isLoading = false;
+      state.error = null;
     },
     deleteItemSuccess(state) {
-      state.isLoading = false
+      state.isLoading = false;
+      state.error = null;
     },
     setFailure: loadingFailed,
-  }
-})
+  },
+});
 
 export const {
   setIsLoading,
@@ -73,7 +86,7 @@ export const {
   patchItemSuccess,
   setProgStatusSuccess,
   deleteItemSuccess,
-  setFailure
+  setFailure,
 } = patchOrderSlice.actions;
 
 export default patchOrderSlice.reducer;
@@ -81,49 +94,54 @@ export default patchOrderSlice.reducer;
 export const patchItem = (id, qty, orderNumber) => async (dispatch) => {
   try {
     dispatch(setIsLoading());
-    dispatch(addLoadingCell({cell: {id: id, orderNumber: orderNumber}}))
+    dispatch(addLoadingCell({ cell: { id: id, orderNumber: orderNumber } }));
     const patchStatus = await patchOrderItem(id, qty);
     if (patchStatus.error) {
-      throw patchStatus.error
+      throw patchStatus.error;
     }
 
-    dispatch(patchItemSuccess({id, orderNumber}));
-  } catch(err) {
-    dispatch(setFailure({ error: err.toString(), cell: {id: id, orderNumber: orderNumber, error: err.toString()} }));
+    dispatch(patchItemSuccess({ id, orderNumber }));
+  } catch (err) {
+    dispatch(
+      setFailure({
+        error: err.toString(),
+        cell: { id: id, orderNumber: orderNumber, error: err.toString() },
+      })
+    );
   }
-}
+};
 
 export const deletePreOrdItem = (id) => async (dispatch) => {
   try {
     dispatch(setIsLoading());
     const deleteStatus = await deletePreOrderItem(id);
     if (deleteStatus.error) {
-      throw deleteStatus.error
+      throw deleteStatus.error;
     }
 
     dispatch(deleteItemSuccess());
-  } catch(err) {
+  } catch (err) {
     dispatch(setFailure({ error: err.toString() }));
   }
-}
+};
 
 export const setProgStatus = (id, value, preOrderId) => async (dispatch) => {
   try {
     dispatch(setIsLoading());
     const compStatus = await setPreOrderProgramStatus(preOrderId, value);
     if (compStatus.error) {
-      throw compStatus.error
+      throw compStatus.error;
     }
-    dispatch(setProgramStatus({ program: id, status: value }))
-    dispatch(setPreOrderStatus({ status: value }))
+    dispatch(setProgramStatus({ program: id, status: value }));
+    dispatch(setPreOrderStatus({ status: value }));
     dispatch(setProgStatusSuccess());
     if (value === "submitted") {
-      const submitStatus = await submitPreOrder(preOrderId)
+      const submitStatus = await submitPreOrder(preOrderId);
       if (submitStatus.error) {
-        throw submitStatus.error
+        throw submitStatus.error;
       }
     }
-  } catch(err) {
+  } catch (err) {
     dispatch(setFailure({ error: err.toString() }));
   }
-}
+};
