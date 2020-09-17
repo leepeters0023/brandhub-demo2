597822928adb1f8a8cf44f5034b2e2530fbcf236
Josh Downs <jdowns@burlingtoncodeacademy.com>
2020-09-17 11:55:53 -0400

    fixed money formatter to handle six figure sums of money properly

--- src/utility/utilityFunctions.js
+++ src/utility/utilityFunctions.js
@@ -89,7 +89,7 @@ export const formatMoney = (value) => {
   if (moneyAr[0].length > 3) {
 
     let preDecimalAr = moneyAr[0].split("").reverse();
-    let formattedPreDec = preDecimalAr.map((num, i) => (i+1)%3===0 ? ","+num : num).reverse().join("")
+    let formattedPreDec = preDecimalAr.map((num, i) => ((i+1)%3===0 && i+1 !== preDecimalAr.length) ? ","+num : num).reverse().join("")
     let returnString = "$"+formattedPreDec+"."+moneyAr[1]
     return returnString;
   } else {
