3f32fadfb2948affbf0f0b54f37aa68c075081c5
Josh Downs <jdowns@burlingtoncodeacademy.com>
2020-09-22 16:16:07 -0400

    added order approval page and order approval table, ready for data after api integrations

---
+++ src/components/OrderHistory/OrderApprovalTable.js
@@ -0,0 +1,233 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { navigate } from "@reach/router";
+import format from "date-fns/format";
+
+import { formatMoney } from "../../utility/utilityFunctions";
+
+import Table from "@material-ui/core/Table";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableBody from "@material-ui/core/TableBody";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import TableSortLabel from "@material-ui/core/TableSortLabel";
+import TableCell from "@material-ui/core/TableCell";
+import Typography from "@material-ui/core/Typography";
+import Tooltip from "@material-ui/core/Tooltip";
+import IconButton from "@material-ui/core/IconButton";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { makeStyles } from "@material-ui/core/styles";
+
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import CancelIcon from '@material-ui/icons/Cancel';
+
+const headCells = [
+  { id: "orderNum", disablePadding: false, label: "Order #", sort: true },
+  { id: "user", disablePadding: false, label: "User", sort: true },
+  {
+    id: "distributor",
+    disablePadding: false,
+    label: "Distributor",
+    sort: true,
+  },
+  { id: "orderDate", disablePadding: false, label: "Order Date", sort: true },
+  {
+    id: "totalItems",
+    disablePadding: false,
+    label: "Total Items",
+    sort: false,
+  },
+  {
+    id: "estTotal",
+    disablePadding: false,
+    label: "Est. Total",
+    sort: false,
+  },
+  { id: "actions", disablePadding: false, label: "", sort: false }
+]
+
+const EnhancedTableHead = (props) => {
+  const { classes, order, orderBy, onRequestSort } = props;
+  const createSortHandler = (property) => (event) => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableHead>
+      <TableRow>
+        {headCells.map((headCell) => {
+          if (!headCell.sort) {
+            return (
+              <TableCell
+                className={classes.headerText}
+                key={headCell.id}
+                align="left"
+              >
+                {headCell.label}
+              </TableCell>
+            );
+          } else {
+            return (
+              <TableCell
+                className={classes.headerText}
+                key={headCell.id}
+                align="left"
+                padding={headCell.disablePadding ? "none" : "default"}
+                sortDirection={orderBy === headCell.id ? order : false}
+              >
+                <TableSortLabel
+                  active={orderBy === headCell.id}
+                  direction={orderBy === headCell.id ? order : "asc"}
+                  onClick={createSortHandler(headCell.id)}
+                >
+                  {headCell.label}
+                  {orderBy === headCell.id ? (
+                    <span className={classes.visuallyHidden}>
+                      {order === "desc"
+                        ? "sorted descending"
+                        : "sorted ascending"}
+                    </span>
+                  ) : null}
+                </TableSortLabel>
+              </TableCell>
+            );
+          }
+        })}
+      </TableRow>
+    </TableHead>
+  );
+};
+
+EnhancedTableHead.propTypes = {
+  classes: PropTypes.object.isRequired,
+  order: PropTypes.oneOf(["asc", "desc"]).isRequired,
+  orderBy: PropTypes.string.isRequired,
+  onRequestSort: PropTypes.func.isRequired,
+};
+
+const useStyles = makeStyles((theme) => ({
+  ...theme.global,
+  orderHistoryRow: {
+    "&&:hover": {
+      cursor: "pointer",
+    },
+  },
+  visuallyHidden: {
+    border: 0,
+    clip: "rect(0 0 0 0)",
+    height: 1,
+    margin: -1,
+    overflow: "hidden",
+    padding: 0,
+    position: "absolute",
+    top: 20,
+    width: 1,
+  },
+}));
+
+const OrderApprovalTable = ({
+  orders,
+  handleSort,
+  isOrdersLoading,
+  scrollRef,
+}) => {
+  const classes = useStyles();
+  const [order, setOrder] = useState("asc");
+  const [orderBy, setOrderBy] = useState("orderDate");
+
+  const handleRequestSort = (_event, property) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+    handleSort({ order: isAsc ? "desc" : "asc", orderBy: property });
+  };
+
+  const handleRowClick = (orderNum) => {
+    navigate(`/orders/open/instock/${orderNum}`);
+  };
+
+  return (
+    <>
+      <TableContainer
+        className={classes.tableContainer}
+        style={{ maxHeight: "Calc(100vh - 300px)" }}
+        ref={scrollRef}
+      >
+        <Table stickyHeader className={classes.table}>
+          <EnhancedTableHead
+            classes={classes}
+            order={order}
+            orderBy={orderBy}
+            onRequestSort={handleRequestSort}
+          />
+          <TableBody>
+            {!isOrdersLoading && orders.length === 0 && (
+              <TableRow>
+                <TableCell align="left" colSpan={11}>
+                  <Typography className={classes.headerText}>
+                    {`You currently don't have any orders on record that match this search criteria..`}
+                  </Typography>
+                </TableCell>
+              </TableRow>
+            )}
+            {!isOrdersLoading &&
+              orders.length > 0 &&
+              orders.map((row) => (
+                <TableRow
+                  key={row.orderNum}
+                  hover
+                  className={classes.orderHistoryRow}
+                  onClick={() => {
+                    handleRowClick(row.orderNum);
+                  }}
+                >
+                  <TableCell align="left">{row.orderNum}</TableCell>
+                  <TableCell align="left">{row.user}</TableCell>
+                  <TableCell align="left">{row.disributor}</TableCell>
+                  <TableCell align="left">
+                    {format(new Date(row.orderDate), "MM/dd/yyyy")}
+                  </TableCell>
+                  <TableCell align="left">{row.totalItems}</TableCell>
+                  <TableCell align="left">
+                    {row.estTotal !== "---"
+                      ? formatMoney(row.estTotal)
+                      : row.estTotal}
+                  </TableCell>
+                  <TableCell align="left">
+                    <div style={{display: "flex", alignItems: "center"}}>
+                      <Tooltip title="Approve">
+                        <IconButton>
+                          <ThumbUpIcon color="inherit" />
+                        </IconButton>
+                      </Tooltip>
+                      <Tooltip title="Deny">
+                        <IconButton>
+                          <CancelIcon color="inherit" />
+                        </IconButton>
+                      </Tooltip>
+                    </div>
+                  </TableCell>
+                </TableRow>
+              ))}
+            {isOrdersLoading && (
+              <TableRow>
+                <TableCell align="left" colSpan={7}>
+                  <CircularProgress />
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+};
+
+OrderApprovalTable.propTypes = {
+  orders: PropTypes.array,
+  handleSort: PropTypes.func.isRequired,
+  isOrdersLoading: PropTypes.bool.isRequired,
+  scrollRef: PropTypes.any.isRequired,
+};
+
+export default OrderApprovalTable;
---
+++ src/pages/OrderApprovals.js
@@ -0,0 +1,420 @@
+import React, { useEffect, useState, useCallback } from "react";
+import "date-fns";
+import subDays from "date-fns/subDays";
+import format from "date-fns/format";
+import { useBottomScrollListener } from "react-bottom-scroll-listener";
+
+import { useSelector, useDispatch } from "react-redux";
+
+import {
+  fetchFilteredOrderHistory,
+  fetchNextOrderHistory,
+} from "../redux/slices/orderHistorySlice";
+
+import { clearBrands } from "../redux/slices/brandSlice";
+
+import { useDetailedInput } from "../hooks/UtilityHooks";
+
+import BrandAutoComplete from "../components/Utility/BrandAutoComplete";
+import DistributorAutoComplete from "../components/Utility/DistributorAutoComplete";
+import UserAutoComplete from "../components/Utility/UserAutoComplete";
+
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import Tooltip from "@material-ui/core/Tooltip";
+import IconButton from "@material-ui/core/IconButton";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import DateFnsUtils from "@date-io/date-fns";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from "@material-ui/pickers";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import { makeStyles } from "@material-ui/core/styles";
+
+import PrintIcon from "@material-ui/icons/Print";
+import GetAppIcon from "@material-ui/icons/GetApp";
+
+const useStyles = makeStyles((theme) => ({
+  ...theme.global,
+  queryRow: {
+    [theme.breakpoints.down("sm")]: {
+      width: "100%",
+      marginLeft: "0%",
+      display: "flex",
+    },
+    [theme.breakpoints.up("md")]: {
+      width: "85%",
+      marginLeft: "15%",
+      display: "flex",
+    },
+    [theme.breakpoints.up("lg")]: {
+      width: "75%",
+      marginLeft: "25%",
+      display: "flex",
+    },
+  },
+  gridItemContainer: {
+    display: "flex",
+    alignItems: "center",
+  },
+}));
+
+const OrderApprovals = () => {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  const nextLink = useSelector((state) => state.orderHistory.nextLink);
+  const isNextLoading = useSelector(
+    (state) => state.orderHistory.isNextLoading
+  );
+
+  const handleBottomScroll = () => {
+    if (nextLink && !isNextLoading) {
+      dispatch(fetchNextOrderHistory(nextLink));
+    }
+  };
+
+  const scrollRef = useBottomScrollListener(handleBottomScroll);
+
+  const [reset, setReset] = useCallback(useState(false));
+
+  const [currentFilters, setCurrentFilters] = useState({
+    fromDate: format(subDays(new Date(), 7), "MM/dd/yyyy"),
+    toDate: format(new Date(), "MM/dd/yyyy"),
+    user: null,
+    distributor: null,
+    program: "",
+    brand: null,
+    sequenceNum: "",
+    sortOrder: "asc",
+    sortOrderBy: "user",
+  });
+
+  const handleFilters = useCallback(
+    (value, type) => {
+      if (type === "program" || type === "sequenceNum") {
+        setCurrentFilters({
+          ...currentFilters,
+          [`${type}`]: value,
+        });
+      } else if (type === "fromDate" || type === "toDate") {
+        setCurrentFilters({
+          ...currentFilters,
+          [`${type}`]: format(value, "MM/dd/yyyy"),
+        });
+      } else if (
+        type === "distributor" ||
+        type === "brand" ||
+        type === "user"
+      ) {
+        setCurrentFilters({
+          ...currentFilters,
+          [`${type}`]: value ? value.id : null,
+        });
+      }
+    },
+    [currentFilters]
+  );
+
+  const {
+    value: sequenceNumber,
+    bind: bindSequenceNumber,
+    reset: resetSequenceNumber,
+  } = useDetailedInput("", handleFilters, "sequenceNum");
+  const {
+    value: program,
+    bind: bindProgram,
+    reset: resetProgram,
+  } = useDetailedInput("", handleFilters, "program");
+
+  const isOrdersLoading = useSelector((state) => state.orderHistory.isLoading);
+  const currentOrders = useSelector((state) => state.orderHistory.orders);
+  const currentUserRole = useSelector((state) => state.user.role);
+
+  const handleSearch = (sortBy = undefined) => {
+    let filterObject;
+    if (sortBy.order) {
+      console.log(sortBy);
+      filterObject = {
+        ...currentFilters,
+        sortOrder: sortBy.order,
+        sortOrderBy: sortBy.orderBy,
+      };
+    } else {
+      console.log(currentFilters);
+      filterObject = { ...currentFilters };
+    }
+    console.log("searching");
+    dispatch(fetchFilteredOrderHistory(filterObject));
+  };
+
+  const handleClearFilters = () => {
+    resetProgram();
+    resetSequenceNumber();
+    setReset(true);
+    //setDistributor(null);
+    dispatch(clearBrands());
+    setCurrentFilters({
+      fromDate: format(subDays(new Date(), 7), "MM/dd/yyyy"),
+      toDate: format(new Date(), "MM/dd/yyyy"),
+      user: null,
+      distributor: null,
+      program: "",
+      brand: null,
+      sequenceNum: "",
+      sortOrder: "asc",
+      sortOrderBy: "user",
+    });
+    dispatch(
+      fetchFilteredOrderHistory({
+        fromDate: format(subDays(new Date(), 7), "MM/dd/yyyy"),
+        toDate: format(new Date(), "MM/dd/yyyy"),
+        user: null,
+        distributor: null,
+        program: "",
+        brand: null,
+        sequenceNum: "",
+        sortOrder: "asc",
+        sortOrderBy: "user",
+      })
+    );
+  };
+
+  const handleSort = (sortObject) => {
+    setCurrentFilters({
+      ...currentFilters,
+      sortOrder: sortObject.order,
+      sortOrderBy: sortObject.orderBy,
+    });
+    handleSearch(sortObject);
+  };
+
+  // useEffect(() => {
+  //   if (currentOrders.length === 0 && currentUserRole.length > 0) {
+  //     dispatch(fetchFilteredOrderHistory(currentFilters));
+  //   }
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
+
+  return (
+    <>
+      <Container className={classes.mainWrapper}>
+        <div className={classes.titleBar}>
+          <Typography className={classes.titleText}>Order History</Typography>
+          <div
+            style={{
+              display: "flex",
+              width: "150px",
+              justifyContent: "flex-end",
+            }}
+          >
+            <Tooltip title="Print Order History">
+              <IconButton>
+                <PrintIcon color="secondary" />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Export CSV">
+              {/* <CSVLink data={currentOrders} headers={csvHeaders}> */}
+              <IconButton>
+                <GetAppIcon color="secondary" />
+              </IconButton>
+              {/* </CSVLink> */}
+            </Tooltip>
+          </div>
+        </div>
+        <br />
+        <div className={classes.queryRow}>
+          <Grid container spacing={2} justify="flex-end">
+            <Grid
+              item
+              
+              md={3}
+              sm={4}
+              className={classes.gridItemContainer}
+            >
+              <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <KeyboardDatePicker
+                  fullWidth
+                  color="secondary"
+                  className={classes.dateField}
+                  disableToolbar
+                  variant="inline"
+                  format="MM/dd/yyyy"
+                  margin="normal"
+                  id="fromDate"
+                  label="Order From Date"
+                  value={currentFilters.fromDate}
+                  onChange={(value) => handleFilters(value, "fromDate")}
+                  KeyboardButtonProps={{
+                    "aria-label": "change date",
+                  }}
+                />
+              </MuiPickersUtilsProvider>
+            </Grid>
+            <Grid
+              item
+              
+              md={3}
+              sm={4}
+              className={classes.gridItemContainer}
+            >
+              <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <KeyboardDatePicker
+                  fullWidth
+                  color="secondary"
+                  className={classes.dateField}
+                  disableToolbar
+                  variant="inline"
+                  format="MM/dd/yyyy"
+                  margin="normal"
+                  id="toDate"
+                  label="Order To Date"
+                  value={currentFilters.toDate}
+                  onChange={(value) => handleFilters(value, "toDate")}
+                  KeyboardButtonProps={{
+                    "aria-label": "change date",
+                  }}
+                />
+              </MuiPickersUtilsProvider>
+            </Grid>
+            <Grid
+              item
+              
+              md={3}
+              sm={4}
+              className={classes.gridItemContainer}
+            >
+              <UserAutoComplete
+                classes={classes}
+                handleChange={handleFilters}
+                reset={reset}
+                setReset={setReset}
+              />
+            </Grid>
+            <Grid
+              item
+              
+              md={3}
+              sm={4}
+              className={classes.gridItemContainer}
+            >
+              <BrandAutoComplete
+                classes={classes}
+                handleChange={handleFilters}
+                reset={reset}
+                setReset={setReset}
+              />
+            </Grid>
+            <Grid
+              item
+              
+              md={3}
+              sm={4}
+              className={classes.gridItemContainer}
+            >
+              <DistributorAutoComplete
+                classes={classes}
+                handleChange={handleFilters}
+                reset={reset}
+                setReset={setReset}
+              />
+            </Grid>
+            <Grid
+              item
+              
+              md={3}
+              sm={4}
+              className={classes.gridItemContainer}
+            >
+              <TextField
+                className={classes.queryField}
+                color="secondary"
+                fullWidth
+                name="program"
+                type="text"
+                label="Program"
+                value={program}
+                {...bindProgram}
+                variant="outlined"
+                size="small"
+              />
+            </Grid>
+            <Grid
+              item
+              
+              md={3}
+              sm={4}
+              className={classes.gridItemContainer}
+            >
+              <TextField
+                className={classes.queryField}
+                color="secondary"
+                fullWidth
+                name="sequenceNumber"
+                type="text"
+                label="Sequence #"
+                value={sequenceNumber}
+                {...bindSequenceNumber}
+                variant="outlined"
+                size="small"
+              />
+            </Grid>
+            <Grid
+              item
+              
+              md={3}
+              sm={4}
+              className={classes.gridItemContainer}
+            >
+              <Button
+                fullWidth
+                className={classes.largeButton}
+                variant="contained"
+                color="secondary"
+                onClick={handleSearch}
+              >
+                SEARCH
+              </Button>
+            </Grid>
+            <Grid
+              item
+              
+              md={3}
+              sm={4}
+              className={classes.gridItemContainer}
+            >
+              <Button
+                fullWidth
+                className={classes.largeButton}
+                variant="contained"
+                color="secondary"
+                onClick={handleClearFilters}
+              >
+                CLEAR FILTERS
+              </Button>
+            </Grid>
+          </Grid>
+        </div>
+        <br />
+        <br />
+        {/* <OrderHistoryTable
+          orders={currentOrders}
+          isOrdersLoading={isOrdersLoading}
+          handleSort={handleSort}
+          scrollRef={scrollRef}
+        /> */}
+        {isNextLoading && (
+          <div style={{ width: "100%" }}>
+            <LinearProgress />
+          </div>
+        )}
+      </Container>
+      <br />
+    </>
+  );
+};
+
+export default OrderApprovals;
