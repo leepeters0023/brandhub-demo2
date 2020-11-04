46168992446da798e938dca1249033d42ab233f0
Josh Downs <jdowns@burlingtoncodeacademy.com>
2020-11-04 17:16:16 -0500

    backbone for new supplier portal is up, navigation is live, need to bring in real statuses

---
+++ src/components/Navigation/DrawerPONav.js
@@ -0,0 +1,92 @@
+import React from "react";
+import { Link } from "@reach/router";
+import PropTypes from "prop-types";
+
+import Grid from "@material-ui/core/Grid";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+
+//TODO get actual number of each status to display from redux (mock only)
+
+const DrawerOrdersNav = ({
+  handleDrawerClose,
+  classes,
+}) => {
+  return (
+    <Grid container spacing={2}>
+      <Grid item sm={3} xs={12}>
+        <List className={classes.navList}>
+          <ListItem
+            button
+            onClick={handleDrawerClose}
+            component={Link}
+            to="/purchasing/poHistory#new"
+          >
+            <ListItemAvatar>
+              <Avatar>
+                5
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary="New" />
+          </ListItem>
+        </List>
+      </Grid>
+      <Grid item sm={3} xs={12}>
+        <List className={classes.navList}>
+        <ListItem
+            button
+            onClick={handleDrawerClose}
+            component={Link}
+            to="/purchasing/poHistory#current"
+          >
+            <ListItemAvatar>
+              <Avatar>
+                10
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary="In Progress" />
+          </ListItem>
+        </List>
+      </Grid>
+      <Grid item sm={3} xs={12}>
+        <List className={classes.navList}>
+        <ListItem
+            button
+            onClick={handleDrawerClose}
+            component={Link}
+            to="/purchasing/poHistory#hold"
+          >
+            <ListItemAvatar>
+              <Avatar>
+                2
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary="Shipping Hold" />
+          </ListItem>
+        </List>
+      </Grid>
+      <Grid item sm={3} xs={12}>
+        <List className={classes.navList}>
+        <ListItem
+            button
+            onClick={handleDrawerClose}
+            component={Link}
+            to="/purchasing/poHistory#all"
+          >
+            <ListItemText primary="History" />
+          </ListItem>
+        </List>
+      </Grid>
+    </Grid>
+  );
+};
+
+DrawerOrdersNav.propTypes = {
+  handleDrawerClose: PropTypes.func.isRequired,
+  classes: PropTypes.object.isRequired,
+};
+
+export default DrawerOrdersNav;
