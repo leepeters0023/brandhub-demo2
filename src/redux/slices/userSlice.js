e4b05963e7a5634f475bdc350439462b62b6fd93
Josh Downs <jdowns@burlingtoncodeacademy.com>
2020-11-05 15:39:39 -0500

    updated distributor favorite lists to handle multiple lists

--- src/redux/slices/userSlice.js
+++ src/redux/slices/userSlice.js
@@ -21,29 +21,32 @@ user: {
 let favoriteItems = [
   {
     id: "1",
-    imgUrl: "https://res.cloudinary.com/joshdowns-dev/image/upload/v1600100945/Select/110011067_Large_1-Case_Talker_vbfgnx.jpg",
+    imgUrl:
+      "https://res.cloudinary.com/joshdowns-dev/image/upload/v1600100945/Select/110011067_Large_1-Case_Talker_vbfgnx.jpg",
     itemNumber: "0110006615",
     itemType: "Digital Banner",
     program: "Apothic Winter 2021",
-    estCost: 299
+    estCost: 299,
   },
   {
     id: "2",
-    imgUrl: "https://res.cloudinary.com/joshdowns-dev/image/upload/v1600100945/Select/110008964_Large_1-carton_rider_p7c8oo.jpg",
+    imgUrl:
+      "https://res.cloudinary.com/joshdowns-dev/image/upload/v1600100945/Select/110008964_Large_1-carton_rider_p7c8oo.jpg",
     itemNumber: "0110006617",
     itemType: "Case Sleeves",
     program: "Apothic General",
-    estCost: 599
+    estCost: 599,
   },
   {
     id: "3",
-    imgUrl: "https://res.cloudinary.com/joshdowns-dev/image/upload/v1595013432/Select/bh_newamsterdam_glorifier_g7orb2.jpg",
+    imgUrl:
+      "https://res.cloudinary.com/joshdowns-dev/image/upload/v1595013432/Select/bh_newamsterdam_glorifier_g7orb2.jpg",
     itemNumber: "0110004612",
     itemType: "Advertising Specialties",
     program: "New Amsterdam Fall 2021",
-    estCost: 550
-  }
-]
+    estCost: 550,
+  },
+];
 
 let initialState = {
   loginIsLoading: false,
@@ -106,7 +109,8 @@ const userSlice = createSlice({
       state.email = user.email;
       state.role = user.role;
       state.territories = [...user.territories];
-      state.managedUsers = user.managedUsers.length > 0 ? [...user.managedUsers] : [];
+      state.managedUsers =
+        user.managedUsers.length > 0 ? [...user.managedUsers] : [];
       state.currentTerritory = user.currentTerritory;
       state.isLoading = false;
       state.loggedIn = true;
@@ -117,9 +121,63 @@ const userSlice = createSlice({
       const { territory } = action.payload;
       state.currentTerritory = territory;
     },
+    createNewFavoriteDistList(state, action) {
+      //TODO get id from api call (async redux thunk call)
+      const { id } = action.payload;
+      let newDistributorList = {
+        id: id,
+        name: "",
+        distributors: [],
+      };
+      let updatedLists = state.favoriteDistributors.concat([
+        newDistributorList,
+      ]);
+      state.favoriteDistributors = updatedLists;
+    },
+    updateDistributorListName(state, action) {
+      const { id, value } = action.payload;
+      let updatedDistributors = state.favoriteDistributors.map((distList) => {
+        if (distList.id === id) {
+          return {
+            ...distList,
+            name: value,
+          };
+        } else return { ...distList };
+      });
+      state.favoriteDistributors = updatedDistributors;
+    },
     setDistributors(state, action) {
-      const { distributors } = action.payload;
-      state.favoriteDistributors = distributors;
+      const { distributors, id } = action.payload;
+      let updatedDistributors = state.favoriteDistributors.map((distList) => {
+        if (distList.id === id) {
+          return {
+            ...distList,
+            distributors: [...distributors],
+          };
+        } else return { ...distList };
+      });
+      state.favoriteDistributors = updatedDistributors;
+    },
+    deleteSingleDistributor(state, action) {
+      const { id, distId } = action.payload;
+      let updatedDistributors = state.favoriteDistributors.map((distList) => {
+        if (distList.id === id) {
+          return {
+            ...distList,
+            distributors: distList.distributors.filter(
+              (dist) => dist.id !== distId
+            ),
+          };
+        } else return { ...distList };
+      });
+      state.favoriteDistributors = updatedDistributors;
+    },
+    deleteDistributorList(state, action) {
+      const { id } = action.payload;
+      let updatedDistributors = state.favoriteDistributors.filter(
+        (distList) => distList.id !== id
+      );
+      state.favoriteDistributors = updatedDistributors;
     },
     removeUser: (state) => {
       state.isLoading = false;
@@ -146,7 +204,11 @@ export const {
   getUserSuccess,
   setLoginSuccess,
   updateCurrentTerritory,
+  createNewFavoriteDistList,
+  updateDistributorListName,
   setDistributors,
+  deleteSingleDistributor,
+  deleteDistributorList,
   removeUser,
   setLogInFailure,
   setFailure,
@@ -161,10 +223,7 @@ export const fetchUser = () => async (dispatch) => {
     if (user.error) {
       throw user.error;
     }
-    window.localStorage.setItem(
-      "brandhub-role",
-      user.data.role
-    );
+    window.localStorage.setItem("brandhub-role", user.data.role);
     let currentUser = {
       id: user.data.id,
       firstName: user.data.name.split(" ")[0],
@@ -174,21 +233,29 @@ export const fetchUser = () => async (dispatch) => {
       }`,
       email: user.data.email,
       role: user.data.role,
-      territories: user.data.territories.length > 0 ? user.data.territories.map((terr) => ({
-        name: terr.name, id: terr.id
-      })) : [],
-      managedUsers: user.data["managed-users"].length > 0 ? user.data["managed-users"].map((u) => ({
-        name: u.name,
-        email: u.email,
-        role: u.role,
-        id: u.id
-      })) : [],
-      currentTerritory: user.data.territories.length > 0 ? user.data.territories[0].id : null
+      territories:
+        user.data.territories.length > 0
+          ? user.data.territories.map((terr) => ({
+              name: terr.name,
+              id: terr.id,
+            }))
+          : [],
+      managedUsers:
+        user.data["managed-users"].length > 0
+          ? user.data["managed-users"].map((u) => ({
+              name: u.name,
+              email: u.email,
+              role: u.role,
+              id: u.id,
+            }))
+          : [],
+      currentTerritory:
+        user.data.territories.length > 0 ? user.data.territories[0].id : null,
     };
     dispatch(getUserSuccess({ user: currentUser }));
   } catch (err) {
     dispatch(setFailure({ error: err.toString }));
-    console.log(err)
+    console.log(err);
   }
 };
 
