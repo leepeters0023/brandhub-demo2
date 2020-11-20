f688f92987ee2a9f482cc4dd4f0493cca9b9d238
leepeters <lpeters0023@gmail.com>
2020-11-20 13:49:16 -0500

    hides supplier column on po history table if role===supplier

--- src/components/SupplierManagement/RFQHistoryTable.js
+++ src/components/SupplierManagement/RFQHistoryTable.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { navigate } from "@reach/router";
 import format from "date-fns/format";
