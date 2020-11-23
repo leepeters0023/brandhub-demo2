ad5f4e8d88a4ca95cc1e05bd932d18ef8b6c78b8
Josh Downs <jdowns@burlingtoncodeacademy.com>
2020-11-23 16:44:41 -0500

    patch request for po dates and note working

--- src/pages/PurchaseOrder.js
+++ src/pages/PurchaseOrder.js
@@ -47,12 +47,12 @@ const PurchaseOrder = ({ handleFiltersClosed }) => {
 
   useEffect(() => {
     if (currentPO.id && !isPOLoading && window.location.hash === "#new") {
-      console.log("hash update")
+      console.log("hash update");
       window.location.hash = currentPO.id;
       setIsNew(true);
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentPO.id])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentPO.id]);
 
   useEffect(() => {
     if (!currentPO.id && window.location.hash !== "#new") {
@@ -61,8 +61,15 @@ const PurchaseOrder = ({ handleFiltersClosed }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  if (isPOLoading || !currentPO.id) {
-    return <Loading />
+  // useEffect(() => {
+  //   if (currentPO.id && window.location.hash !== "#new" && currentPO.id !== window.location.hash.slice(1)) {
+  //     dispatch(fetchSinglePO(window.location.hash.slice(1)));
+  //   }
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
+
+  if (isPOLoading) {
+    return <Loading />;
   }
 
   return (
@@ -96,9 +103,9 @@ const PurchaseOrder = ({ handleFiltersClosed }) => {
                 </IconButton>
               </Tooltip>
             )}
-          <Typography className={classes.titleText}>
-            {`Purchase Order #${currentPO.id}`}
-          </Typography>
+            <Typography className={classes.titleText}>
+              {`Purchase Order #${currentPO.id}`}
+            </Typography>
           </div>
           <div className={classes.configButtons}>
             <div className={classes.innerConfigDiv}>
@@ -120,17 +127,7 @@ const PurchaseOrder = ({ handleFiltersClosed }) => {
           }}
         >
           <CurrentPO currentPO={currentPO} />
-          {window.location.hash.includes("new") && (
-            <div>
-              <Button
-                className={classes.largeButton}
-                variant="contained"
-                color="secondary"
-              >
-                SUBMIT PURCHASE ORDER
-              </Button>
-            </div>
-          )}
+
           <br />
           {(window.location.hash.includes("new") ||
             currentPO.status === "draft") && (
