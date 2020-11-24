109fe8c7c721a37bfecf5938ca40d17dd32c65c9
Lee Peters <lpeters0023@gmail.com>
2020-11-24 16:42:51 -0500

    increases drawer height to accomodate + New Coupon if !Field1

--- src/components/Navigation/TopDrawerNav.js
+++ src/components/Navigation/TopDrawerNav.js
@@ -37,7 +37,7 @@ const useStyles = makeStyles((theme) => ({
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
-    height: "325px",
+    height: "345px",
     overflowY: "hidden",
   },
   drawerSupplierOpen: {
