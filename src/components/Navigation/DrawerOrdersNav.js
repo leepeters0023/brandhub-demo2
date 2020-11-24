efb4556474b5c369744128d7008b3d249aecac87
Lee Peters <lpeters0023@gmail.com>
2020-11-24 16:42:51 -0500

    increases drawer height to accomodate + New Coupon if !Field1

--- src/components/Navigation/DrawerOrdersNav.js
+++ src/components/Navigation/DrawerOrdersNav.js
@@ -16,157 +16,159 @@ const DrawerOrdersNav = ({
   handleCouponModal,
 }) => {
   return (
-    <Grid container spacing={2}>
-      <Grid item sm={3} xs={12}>
-        <List className={classes.navList}>
-          <ListItem>
-            <ListItemText
-              primaryTypographyProps={{ className: classes.headerText }}
-              primary="Place an Order:"
-            />
-          </ListItem>
-          {role === "field1" && (
-            <ListItem
-              button
-              onClick={handleDrawerClose}
-              component={Link}
-              to="/orders/open/preorder"
-            >
-              <ListItemText primary="Quarterly Pre-Order" />
-            </ListItem>
-          )}
-          <ListItem
-            button
-            onClick={handleDrawerClose}
-            component={Link}
-            to="/orders/items/inStock"
-          >
-            <ListItemText primary="+ New In-Stock Order" />
-          </ListItem>
-          <ListItem
-            button
-            onClick={handleDrawerClose}
-            component={Link}
-            to="/orders/items/onDemand"
-          >
-            <ListItemText primary="+ New On-Demand Order" />
-          </ListItem>
+    <>
+      {role !== "compliance" && (
+        <Grid container spacing={2}>
+          <Grid item sm={3} xs={12}>
+            <List className={classes.navList}>
+              <ListItem>
+                <ListItemText
+                  primaryTypographyProps={{ className: classes.headerText }}
+                  primary="Place an Order:"
+                />
+              </ListItem>
+              <ListItem
+                button
+                onClick={handleDrawerClose}
+                component={Link}
+                to="/orders/open/preorder"
+              >
+                <ListItemText primary="+ Quarterly Pre-Order" />
+              </ListItem>
+              <ListItem
+                button
+                onClick={handleDrawerClose}
+                component={Link}
+                to="/orders/items/onDemand"
+              >
+                <ListItemText primary="+ New On-Demand Order" />
+              </ListItem>
+              <ListItem
+                button
+                onClick={handleDrawerClose}
+                component={Link}
+                to="/orders/items/inStock"
+              >
+                <ListItemText primary="+ New Inventory Order" />
+              </ListItem>
+              {role !== "field1" && (
+                <ListItem
+                  button
+                  onClick={() => {
+                    handleDrawerClose()
+                    handleCouponModal()
+                  }}
+                >
+                  <ListItemText
+                    primary="+ New Coupon"
+                  />
+                </ListItem>
+              )}
+            </List>
+          </Grid>
+          {role === "field1" && <Grid item sm={1} xs={12} />}
+          <Grid item sm={role === "field1" ? 4 : 3} xs={12}>
+            <List className={classes.navList}>
+              <ListItem>
+                <ListItemText
+                  primaryTypographyProps={{ className: classes.headerText }}
+                  primary="Draft Orders:"
+                />
+              </ListItem>
+              <ListItem
+                button
+                onClick={handleDrawerClose}
+                component={Link}
+                to={
+                  inStockOrderId
+                    ? `/orders/open/${inStockOrderId}`
+                    : "/orders/open/inStock"
+                }
+              >
+                <ListItemText primary="In-Stock" />
+              </ListItem>
+              <ListItem
+                button
+                onClick={handleDrawerClose}
+                component={Link}
+                to={
+                  onDemandOrderId
+                    ? `/orders/open/${onDemandOrderId}`
+                    : "/orders/open/onDemand"
+                }
+              >
+                <ListItemText primary="On-Demand" />
+              </ListItem>
+            </List>
+          </Grid>
           {role !== "field1" && (
-            <ListItem
-              button
-              onClick={() => {
-                handleDrawerClose()
-                handleCouponModal()
-              }}
-            >
-              <ListItemText
-                primary="+ New Coupon"
-              />
-            </ListItem>
+            <Grid item sm={3} xs={12}>
+              <List className={classes.navList}>
+                <ListItem>
+                  <ListItemText
+                    primaryTypographyProps={{ className: classes.headerText }}
+                    primary="Order Review:"
+                  />
+                </ListItem>
+                <ListItem
+                  button
+                  onClick={handleDrawerClose}
+                  component={Link}
+                  to="/rollup"
+                >
+                  <ListItemText primary="Quarterly Rollup" />
+                </ListItem>
+                <ListItem
+                  button
+                  onClick={handleDrawerClose}
+                  component={Link}
+                  to="/orders/approvals"
+                >
+                  <ListItemText primary="Approvals" />
+                </ListItem>
+              </List>
+            </Grid>
           )}
-        </List>
-      </Grid>
-      {role === "field1" && <Grid item sm={1} xs={12} />}
-      <Grid item sm={role === "field1" ? 4 : 3} xs={12}>
-        <List className={classes.navList}>
-          <ListItem>
-            <ListItemText
-              primaryTypographyProps={{ className: classes.headerText }}
-              primary="Draft Orders:"
-            />
-          </ListItem>
-          <ListItem
-            button
-            onClick={handleDrawerClose}
-            component={Link}
-            to={
-              inStockOrderId
-                ? `/orders/open/${inStockOrderId}`
-                : "/orders/open/inStock"
-            }
-          >
-            <ListItemText primary="In-Stock" />
-          </ListItem>
-          <ListItem
-            button
-            onClick={handleDrawerClose}
-            component={Link}
-            to={
-              onDemandOrderId
-                ? `/orders/open/${onDemandOrderId}`
-                : "/orders/open/onDemand"
-            }
-          >
-            <ListItemText primary="On-Demand" />
-          </ListItem>
-        </List>
-      </Grid>
-      {role !== "field1" && (
-        <Grid item sm={3} xs={12}>
-          <List className={classes.navList}>
-            <ListItem>
-              <ListItemText
-                primaryTypographyProps={{ className: classes.headerText }}
-                primary="Order Review:"
-              />
-            </ListItem>
-            <ListItem
-              button
-              onClick={handleDrawerClose}
-              component={Link}
-              to="/rollup"
-            >
-              <ListItemText primary="Quarterly Rollup" />
-            </ListItem>
-            <ListItem
-              button
-              onClick={handleDrawerClose}
-              component={Link}
-              to="/orders/approvals"
-            >
-              <ListItemText primary="Approvals" />
-            </ListItem>
-          </List>
+          <Grid item sm={role === "field1" ? 4 : 3} xs={12}>
+            <List className={classes.navList}>
+              <ListItem>
+                <ListItemText
+                  primaryTypographyProps={{ className: classes.headerText }}
+                  primary="Reporting:"
+                />
+              </ListItem>
+              <ListItem
+                button
+                onClick={handleDrawerClose}
+                component={Link}
+                to="/orders/history"
+              >
+                <ListItemText primary="Order History" />
+              </ListItem>
+              <ListItem
+                button
+                onClick={handleDrawerClose}
+                component={Link}
+                to="/budgets/ytod"
+              >
+                <ListItemText primary="Budget vs. Spend (YtoD)" />
+              </ListItem>
+              <ListItem
+                button
+                onClick={handleDrawerClose}
+                component={Link}
+                to="/"
+              >
+                <ListItemText
+                  style={{ fontStyle: "italic" }}
+                  primary="* Inventory"
+                />
+              </ListItem>
+            </List>
+          </Grid>
         </Grid>
       )}
-      <Grid item sm={role === "field1" ? 4 : 3} xs={12}>
-        <List className={classes.navList}>
-          <ListItem>
-            <ListItemText
-              primaryTypographyProps={{ className: classes.headerText }}
-              primary="Reporting:"
-            />
-          </ListItem>
-          <ListItem
-            button
-            onClick={handleDrawerClose}
-            component={Link}
-            to="/orders/history"
-          >
-            <ListItemText primary="Order History" />
-          </ListItem>
-          <ListItem
-            button
-            onClick={handleDrawerClose}
-            component={Link}
-            to="/budgets/ytod"
-          >
-            <ListItemText primary="Budget vs. Spend (YtoD)" />
-          </ListItem>
-          <ListItem
-              button
-              onClick={handleDrawerClose}
-              component={Link}
-              to="/"
-            >
-              <ListItemText
-                style={{fontStyle: "italic"}}
-                primary="* Inventory"
-              />
-            </ListItem>
-        </List>
-      </Grid>
-    </Grid>
+    </>
   );
 };
 
