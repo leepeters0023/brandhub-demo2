11d70f1828692f52a1d416475de869d0d65a6d83
Josh Downs <jdowns@burlingtoncodeacademy.com>
2020-11-30 09:12:08 -0500

    switched fav items request to a put, removed id

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -68,7 +68,7 @@ export const addFavoriteItems = async (idArray) => {
   const ids = idArray.map((id => ({ type: "item", id: id})))
   console.log(ids)
   await axios
-    .patch("/api/current-user/123456", {
+    .put("/api/current-user", {
       data: {
         type: "current-user",
         relationships: {
