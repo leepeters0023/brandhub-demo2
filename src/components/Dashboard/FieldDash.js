a9ed36aac5e2c0726ec2979f741457010ac119ff
Lee Peters <lpeters0023@gmail.com>
2021-01-04 12:54:56 -0500

    adds info popover to next 4 cards

--- src/components/Dashboard/FieldDash.js
+++ src/components/Dashboard/FieldDash.js
@@ -24,6 +24,11 @@ const FieldDash = ({ classes, InfoPopover }) => {
   const inStockOpen = anchorEl ? anchorEl.id === "in-stock-parent" : false;
   const onDemandOpen = anchorEl ? anchorEl.id === "on-demand-parent" : false;
   const historyOpen = anchorEl ? anchorEl.id === "history-parent" : false;
+  const catalogCurrentOpen = anchorEl ? anchorEl.id === "catalog-current-parent" : false;
+  const catalogArchiveOpen = anchorEl ? anchorEl.id === "catalog-archive-parent" : false;
+  const itemRulesOpen = anchorEl ? anchorEl.id === "item-rules-parent" : false;
+  const wrapReportOpen = anchorEl ? anchorEl.id === "wrap-report-parent" : false;
+
   const cardData = [
     {
       titleText: "Q1 Ordering",
@@ -34,65 +39,74 @@ const FieldDash = ({ classes, InfoPopover }) => {
       info: "Place your Quarter One Pre-Order. Orders are placed in groups based on programs and distributors that are availiable within your assigned Regions and Key Accounts",
     },
     {
-      titleText: "Q1 Ordering",
-      ariaOwnsState: preOrderOpen,
-      ariaOwnsText: "pre-order",
-      id: "pre-order-parent",
-      link: "/programs",
-      info: "Place your Quarter One Pre-Order. Orders are placed in groups based on programs and distributors that are availiable within your assigned Regions and Key Accounts",
+      titleText: "Place On-Demand Order",
+      ariaOwnsState: onDemandOpen,
+      ariaOwnsText: "on-demand",
+      id: "on-demand-parent",
+      link: "/orders/items/onDemand",
+      info: "Place orders for items that will need to be produced for your order",
     },
     {
-      titleText: "Q1 Ordering",
-      ariaOwnsState: preOrderOpen,
-      ariaOwnsText: "pre-order",
-      id: "pre-order-parent",
-      link: "/programs",
-      info: "Place your Quarter One Pre-Order. Orders are placed in groups based on programs and distributors that are availiable within your assigned Regions and Key Accounts",
+      titleText: "Place In-Stock Order",
+      ariaOwnsState: inStockOpen,
+      ariaOwnsText: "in-stock",
+      id: "in-stock-parent",
+      link: "/orders/items/inStock",
+      info: "Place orders for items that are currently available in our inventory",
     },
     {
-      titleText: "Q1 Ordering",
-      ariaOwnsState: preOrderOpen,
-      ariaOwnsText: "pre-order",
-      id: "pre-order-parent",
-      link: "/programs",
-      info: "Place your Quarter One Pre-Order. Orders are placed in groups based on programs and distributors that are availiable within your assigned Regions and Key Accounts",
+      titleText: "Order History",
+      ariaOwnsState: historyOpen,
+      ariaOwnsText: "order-history",
+      id: "history-parent",
+      link: "/orders/history/group/byOrder",
+      info: "View order history grouped by item or order & get tracking information",
     },
     {
-      titleText: "Q1 Ordering",
-      ariaOwnsState: preOrderOpen,
-      ariaOwnsText: "pre-order",
-      id: "pre-order-parent",
-      link: "/programs",
-      info: "Place your Quarter One Pre-Order. Orders are placed in groups based on programs and distributors that are availiable within your assigned Regions and Key Accounts",
+      titleText: "Item Catalog: Current",
+      ariaOwnsState: catalogCurrentOpen,
+      ariaOwnsText: "catalog-current",
+      id: "catalog-current-parent",
+      link: "/items/all",
+      info: "View catalog of and details about currently available items",
     },
     {
-      titleText: "Q1 Ordering",
-      ariaOwnsState: preOrderOpen,
-      ariaOwnsText: "pre-order",
-      id: "pre-order-parent",
-      link: "/programs",
-      info: "Place your Quarter One Pre-Order. Orders are placed in groups based on programs and distributors that are availiable within your assigned Regions and Key Accounts",
+      titleText: "Item Catalog: Archive",
+      ariaOwnsState: catalogArchiveOpen,
+      ariaOwnsText: "catalog-archive",
+      id: "catalog-archive-parent",
+      link: "/items/all",
+      //link: "/items/archive"
+      info: "View archive of and details about currently available items",
     },
     {
-      titleText: "Q1 Ordering",
-      ariaOwnsState: preOrderOpen,
-      ariaOwnsText: "pre-order",
-      id: "pre-order-parent",
-      link: "/programs",
-      info: "Place your Quarter One Pre-Order. Orders are placed in groups based on programs and distributors that are availiable within your assigned Regions and Key Accounts",
+      titleText: "Item Rules",
+      ariaOwnsState: itemRulesOpen,
+      ariaOwnsText: "item-rules",
+      id: "item-rules-parent",
+      link: "/compliance/items",
+      info: "View details about regional item compliance",
     },
-      {
-      titleText: "Q1 Ordering",
-      ariaOwnsState: preOrderOpen,
-      ariaOwnsText: "pre-order",
-      id: "pre-order-parent",
-      link: "/programs",
-      info: "Place your Quarter One Pre-Order. Orders are placed in groups based on programs and distributors that are availiable within your assigned Regions and Key Accounts",
+    {
+      titleText: "Wrap Up Report",
+      ariaOwnsState: wrapReportOpen,
+      ariaOwnsText: "wrap-up-report",
+      id: "wrap-report-parent",
+      link: "/reports/wrap-up",
+      info: "View wrap up report",
     },
+    
   ]
 
-  // meeting with Carlton 12/30 cards to include: current preorder (newicon, element of time is good), ondemand order (use ad-hoc icon currently in use), instock order (newicon), order history (newicon question mark, person thinking?), 
-  // current item catalog, archive, item rules, report
+  // meeting with Carlton 12/30 cards to include: 
+  // x current preorder (newicon, element of time is good), 
+  // x ondemand order (use ad-hoc icon currently in use), 
+  // x instock order (newicon), 
+  // x order history (newicon question mark, person thinking?), 
+  // x current item catalog
+  // x archive
+  // item rules
+  // report
   return (
     <Grid container spacing={6} justify="center" style={{ width: "80%" }}>
       {cardData.map((data) => (
@@ -123,137 +137,6 @@ const FieldDash = ({ classes, InfoPopover }) => {
         </Grid>
 
       ))}
-      {/* <Grid item md={3} sm={6} xs={12} component={Link} to="/programs">
-        <div className={classes.dashboardGridItem}>
-          <Paper className={classes.dashPaper}>
-            <div
-              id="pre-order-parent"
-              className={classes.innerPaper}
-              aria-owns={preOrderOpen ? "pre-order" : undefined}
-              aria-haspopup="true"
-              onMouseEnter={handlePopoverOpen}
-              onMouseLeave={handlePopoverClose}
-            >
-              <Typography className={classes.titleText}>Q1 Ordering</Typography>
-            </div>
-          </Paper>
-        </div>
-        <InfoPopover
-          id={"pre-order"}
-          info={
-            "Place your Quarter One Pre-Order. Orders are placed in groups based on programs and distributors that are availiable within your assigned Regions and Key Accounts"
-          }
-          classes={classes}
-          open={preOrderOpen}
-          anchorEl={anchorEl}
-          handlePopoverClose={handlePopoverClose}
-        />
-      </Grid>
-      <Grid
-        item
-        md={3}
-        sm={6}
-        xs={12}
-        component={Link}
-        to="/orders/items/inStock"
-      >
-        <div className={classes.dashboardGridItem}>
-          <Paper className={classes.dashPaper}>
-            <div
-              id="in-stock-parent"
-              className={classes.innerPaper}
-              aria-owns={inStockOpen ? "in-stock" : undefined}
-              aria-haspopup="true"
-              onMouseEnter={handlePopoverOpen}
-              onMouseLeave={handlePopoverClose}
-            >
-              <Typography className={classes.titleText}>
-                Place In-Stock Order
-              </Typography>
-            </div>
-          </Paper>
-        </div>
-        <InfoPopover
-          id={"in-stock"}
-          info={
-            "Place orders for items that are currently available in our inventory"
-          }
-          classes={classes}
-          open={inStockOpen}
-          anchorEl={anchorEl}
-          handlePopoverClose={handlePopoverClose}
-        />
-      </Grid>
-      <Grid
-        item
-        md={3}
-        sm={6}
-        xs={12}
-        component={Link}
-        to="/orders/items/onDemand"
-      >
-        <div className={classes.dashboardGridItem}>
-          <Paper className={classes.dashPaper}>
-            <div
-              id="on-demand-parent"
-              className={classes.innerPaper}
-              aria-owns={onDemandOpen ? "on-demand" : undefined}
-              aria-haspopup="true"
-              onMouseEnter={handlePopoverOpen}
-              onMouseLeave={handlePopoverClose}
-            >
-              <Typography className={classes.titleText}>
-                Place On-Demand Order
-              </Typography>
-            </div>
-          </Paper>
-        </div>
-        <InfoPopover
-          id={"on-demand"}
-          info={
-            "Place orders for items that will need to be produced for your order"
-          }
-          classes={classes}
-          open={onDemandOpen}
-          anchorEl={anchorEl}
-          handlePopoverClose={handlePopoverClose}
-        />
-      </Grid>
-      <Grid
-        item
-        md={3}
-        sm={6}
-        xs={12}
-        component={Link}
-        to="/orders/history/group/byOrder"
-      >
-        <div className={classes.dashboardGridItem}>
-          <Paper className={classes.dashPaper}>
-            <div
-              id="history-parent"
-              className={classes.innerPaper}
-              aria-owns={historyOpen ? "order-history" : undefined}
-              aria-haspopup="true"
-              onMouseEnter={handlePopoverOpen}
-              onMouseLeave={handlePopoverClose}
-            >
-              <Typography className={classes.titleText}>
-                Order History
-              </Typography>
-            </div>
-          </Paper>
-        </div>
-        <InfoPopover
-          id={"order-history"}
-          info={
-            "View order history grouped by item or order & get tracking information"
-          }
-          classes={classes}
-          open={historyOpen}
-          anchorEl={anchorEl}
-          handlePopoverClose={handlePopoverClose}
-        />
-      </Grid> */}
     </Grid>
   );
 };
