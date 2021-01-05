8b1ad11d58f6328c4e864fcc48d2fd1d739125c0
Josh Downs <jdowns@burlingtoncodeacademy.com>
2021-01-05 17:04:46 -0500

    updates various parts of ui to handle new inventory po model, adds warehouse to item model

--- src/redux/slices/purchaseOrderSlice.js
+++ src/redux/slices/purchaseOrderSlice.js
@@ -385,7 +385,6 @@ export const createNewPO = (idArray) => async (dispatch) => {
     if (newPO.error) {
       throw newPO.error;
     }
-    console.log(newPO);
     const formattedPO = mapPurchaseOrder(newPO.data);
     dispatch(getSinglePOSuccess({ purchaseOrder: formattedPO }));
   } catch (err) {
