6ded463368cb7735d2fe59b158489e63ee759255
Josh Downs <jdowns@burlingtoncodeacademy.com>
2021-01-07 11:16:16 -0500

    changes name in api maps to handle api response

--- src/utility/cloudinary.js
+++ src/utility/cloudinary.js
@@ -2,6 +2,5 @@ import { Util } from "cloudinary-core";
 
 export const openUploadWidget = (options, callback) => {
   const scOptions = Util.withSnakeCaseKeys(options);
-  console.log(scOptions)
   window.cloudinary.openUploadWidget(scOptions, callback);
 };
