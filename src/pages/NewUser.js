5b212da1b4460315e67697aece0c818a97754355
Josh Downs <jdowns@burlingtoncodeacademy.com>
2021-01-08 18:07:09 -0500

    adds redirect for new user, needs logout button

---
+++ src/pages/NewUser.js
@@ -0,0 +1,97 @@
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+
+import { useSelector } from "react-redux";
+
+import Logo from "../assets/RTA_Logo_Stacked.png";
+
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  ...theme.global,
+  newUserWrapper: {
+    width: "100vw",
+    height: "100vh",
+    display: "flex",
+    position: "fixed",
+    top: 0,
+    left: 0,
+    overflow: "hidden",
+    backgroundColor: "#FFFFFF",
+  },
+  infoWrapper: {
+    height: "80vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    position: "fixed",
+    top: "20px",
+    left: "20px",
+    width: "150px",
+    height: "auto",
+    filter: "brightness(0%)",
+  },
+}));
+
+const NewUser = ({ handleFiltersClosed }) => {
+  const classes = useStyles();
+
+  const firstName = useSelector((state) => state.user.firstName);
+  const lastName = useSelector((state) => state.user.lastName);
+
+  useEffect(() => {
+    handleFiltersClosed();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className={classes.newUserWrapper}>
+      <img src={Logo} className={classes.logo} alt="Logo" />
+      <Container className={classes.infoWrapper}>
+        <div
+          style={{
+            height: "70vh",
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Grid container>
+            <Grid item sm={2} xs={1} />
+            <Grid item sm={8} xs={10} style={{ textAlign: "center" }}>
+              <Typography className={classes.titleText} variant="h5">
+                {`Welcome to Ready to Activate ${firstName} ${lastName}`}
+              </Typography>
+              <br />
+              <Typography className={classes.headerText}>
+                It looks like your account hasn't been set up yet.
+              </Typography>
+              <br />
+              <Typography className={classes.headerText}>
+                Please contact your manager, or the Ready to Activate Help Desk
+                for assistance.
+              </Typography>
+              <Typography className={classes.headerText}>
+                help@readytoactivate.com
+              </Typography>
+            </Grid>
+            <Grid item sm={2} xs={1} />
+          </Grid>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+NewUser.propTypes = {
+  handleFiltersClosed: PropTypes.func.isRequired,
+};
+
+export default NewUser;
