99f5f9744ea00c061b907d4bc016600b485e5f9b
Lee Peters <lpeters0023@gmail.com>
2021-01-11 13:04:02 -0500

    removes down chevron from autocomplete

--- src/components/Utility/ItemTypeAutoComplete.js
+++ src/components/Utility/ItemTypeAutoComplete.js
@@ -72,14 +72,7 @@ const ItemTypeAutoComplete = ({
               ...params.InputProps,
               autoComplete: "new-password",
               endAdornment: <>
-                <InputAdornment>
-                  <ExpandMoreIcon // not married to the idea of this being here as it's hard to position with "X" clear icon https://github.com/mui-org/material-ui/issues/17799
-                    className={classes.hoverText}
-                    color="secondary"
-                    onMouseEnter={() => setOpen(true)}
-                   />
-                   {params.InputProps.endAdornment}
-                </InputAdornment>
+                {params.InputProps.endAdornment}
               </>,
             }}
           />
