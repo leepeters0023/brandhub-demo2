2d548d2eda9837ba2f30edab61a4d469b44bc13d
Lee Peters <lpeters0023@gmail.com>
2021-01-12 13:36:15 -0500

    fixes html wrapper

--- src/App.js
+++ src/App.js
@@ -227,7 +227,7 @@ const App = () => {
   return (
     <MuiThemeProvider theme={theme}>
       <Helmet>
-        <html lang="en" />
+        
         <meta charset="utf-8" />
         <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
