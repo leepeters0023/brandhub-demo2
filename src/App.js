6e53c66e375e20b05851555369a56ba57665a3c6
Josh Downs <jdowns@burlingtoncodeacademy.com>
2021-01-12 14:38:21 -0500

    fixes manafest and favicon links to not throw errors

--- src/App.js
+++ src/App.js
@@ -229,11 +229,11 @@ const App = () => {
       <Helmet>
         <meta charset="utf-8" />
         <title>Ready to Activate</title>
-        <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
-        <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
-        <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+        <link rel="apple-touch-icon" href="logo192.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta name="insight-app-sec-validation" content="aced815d-d550-4d28-87ed-4b8d32c32430" />
         {isProd && (
             <script type="text/javascript">{`!function(e,t,n){function a(){var e=t.getElementsByTagName("script")[0],n=t.createElement("script");n.type="text/javascript",n.async=!0,n.src="https://beacon-v2.helpscout.net",e.parentNode.insertBefore(n,e)}if(e.Beacon=n=function(t,n,a){e.Beacon.readyQueue.push({method:t,options:n,data:a})},n.readyQueue=[],"complete"===t.readyState)return a();e.attachEvent?e.attachEvent("onload",a):e.addEventListener("load",a,!1)}(window,document,window.Beacon||function(){});window.Beacon('init', '521f5954-7022-46e2-9707-6a82501f23e7')`}</script>
