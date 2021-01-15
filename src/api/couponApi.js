e4fde943550da98909919788f58d9232302fb297
Josh Downs <jdowns@burlingtoncodeacademy.com>
2021-01-15 12:01:54 -0500

    polling function written for order set fetch

--- src/api/couponApi.js
+++ src/api/couponApi.js
@@ -1,4 +1,7 @@
 import axios from "axios";
+import Jsona from "jsona";
+
+const dataFormatter = new Jsona();
 
 const instance = axios.create();
 delete instance.defaults.headers.common["Authorization"];
@@ -33,5 +36,46 @@ export const getCouponUrl = async (email, url) => {
       response.status = "error";
       response.error = err.response.data.errors[0].title;
     });
+  return response;
+};
+
+export const getCouponOrderSet = async (code) => {
+  const response = { status: "", error: null, data: null };
+  let polling = true;
+  const sleep = (ms, cancelToken, cb) => {
+    return new Promise((resolve, reject) => () => {
+      cancelToken.cancel = () => {
+        reject(new Error("sleep() cancelled"))
+      };
+      setTimeout(()=>{
+        cb()
+        resolve()
+      }, ms)
+    })
+  }
+  const breakPoll = () => {
+    polling = false;
+  }
+  const token = {};
+  await sleep(10000, token, breakPoll);
+  const getSet = async () => {
+    await axios
+      .get(`/api/order-sets/coupon/${code}`)
+      .then((res) => {
+        let data = dataFormatter.deserialize(res.data);
+        polling = false
+        response.status = "ok";
+        response.data = data;
+      })
+      .catch((err) => {
+        console.log(err.response.data.errors[0].title);
+        response.status = "error";
+        response.error = err.response.data.errors[0].title;
+      });
+  };
+  await getSet();
+  while (polling && !response.data) {
+    await getSet();
+  }
   return response;
 };
