7552a3d31b27ac01c35fa48391a5da436e64d407
Lee Peters <lpeters0023@gmail.com>
2021-01-18 11:22:24 -0500

    adds new insight-app code

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ ReactDOM.render(
           <link rel="manifest" href="/manifest.json" />
           <meta
             name="insight-app-sec-validation"
-            content="2d6c244f-f6f7-4359-b665-6afb1b69df4b"
+            content="b0e47055-1c1d-42af-ac4c-c21e7b18370e"
           />
           {isProd && (
             <script type="text/javascript">{`!function(e,t,n){function a(){var e=t.getElementsByTagName("script")[0],n=t.createElement("script");n.type="text/javascript",n.async=!0,n.src="https://beacon-v2.helpscout.net",e.parentNode.insertBefore(n,e)}if(e.Beacon=n=function(t,n,a){e.Beacon.readyQueue.push({method:t,options:n,data:a})},n.readyQueue=[],"complete"===t.readyState)return a();e.attachEvent?e.attachEvent("onload",a):e.addEventListener("load",a,!1)}(window,document,window.Beacon||function(){});window.Beacon('init', '521f5954-7022-46e2-9707-6a82501f23e7')`}</script>
