d575cab6b9884c636e94b76a1d79787117952575
Josh Downs <jdowns@burlingtoncodeacademy.com>
2021-01-20 09:49:03 -0500

    update to coupon headers

--- src/api/couponApi.js
+++ src/api/couponApi.js
@@ -31,7 +31,7 @@ export const getCouponUrl = async (email, url) => {
           Accept: "*/*",
           "X-AspNet-Version": "4.0.30319",
           "Access-Control-Allow-Origin": "https://brandhub-ui-dev.onrender.com",
-          "Access-Control-Allow-Headers": "Content-Type, Authorization",
+          "Access-Control-Allow-Headers": "Authorization",
           "Access-Control-Allow-Methods": "HEAD,GET,POST,OPTIONS",
         },
       }
