5bcb35c74ddb5fd97edb4b432bb8ae1240374923
Lee Peters <lpeters0023@gmail.com>
2021-01-26 12:59:28 -0500

    work in progress

--- src/components/Profile/General.js
+++ src/components/Profile/General.js
@@ -1,18 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 
 import { useSelector } from "react-redux";
 
-import { useInput } from "../../hooks/InputHooks";
-
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
+
+import Checkbox from '@material-ui/core/Checkbox';
 
 const useStyles = makeStyles((theme) => ({
   ...theme.global,
-  settingsMargin: {
-    marginBottom: "15px",
+  profileText: {
+    
   }
 }));
 
@@ -20,82 +19,53 @@ const General = () => {
   const classes = useStyles();
 
   const currentUser = useSelector((state) => state.user)
-  
-  const [info, setInfo] = useState({
-    first: currentUser.firstName,
-    last: currentUser.lastName,
-    email: currentUser.email,
-    phone: "999-999-9999",
-  });
+  console.log(currentUser)
+  // const [info, setInfo] = useState({
+  //   first: currentUser.firstName,
+  //   last: currentUser.lastName,
+  //   email: currentUser.email,
+  //   role: currentUser.role,
+  // });
 
-  const { value: firstName, bind: bindFirstName } = useInput(info.first);
-  const { value: lastName, bind: bindLastName } = useInput(info.last);
-  const { value: email, bind: bindEmail } = useInput(info.email);
-  const { value: phone, bind: bindPhone } = useInput(info.phone);
-  const { value: oldPassword, bind: bindOldPassword } = useInput("");
-  const { value: updatePassword, bind: bindUpdatePassword } = useInput("");
-  const { value: confirmPassword, bind: bindConfirmPassword } = useInput("");
+  // const { value: firstName, bind: bindFirstName } = useInput(info.first);
+  // const { value: lastName, bind: bindLastName } = useInput(info.last);
+  // const { value: email, bind: bindEmail } = useInput(info.email);
+  // const { value: role, bind: bindRole } = useInput(info.role);
+  // const { value: oldPassword, bind: bindOldPassword } = useInput("");
+  // const { value: updatePassword, bind: bindUpdatePassword } = useInput("");
+  // const { value: confirmPassword, bind: bindConfirmPassword } = useInput("");
 
-  const handleFormSubmit = (evt) => {
-    evt.preventDefault();
-    //temporarily removing unused vars so build will work on netlify, not permenant
-    console.log(oldPassword, updatePassword, confirmPassword)
-    if (evt.target.id==="profile") {
-      setInfo({
-        first: firstName,
-        last: lastName,
-        email: email,
-        phone: phone
-      })
-    }
-  };
+  // const handleFormSubmit = (evt) => {
+  //   evt.preventDefault();
+  //   //temporarily removing unused vars so build will work on netlify, not permenant
+  //   console.log(oldPassword, updatePassword, confirmPassword)
+  //   if (evt.target.id==="profile") {
+  //     setInfo({
+  //       first: firstName,
+  //       last: lastName,
+  //       email: email,
+  //       role: role,
+  //     })
+  //   }
+  // };
 
   return (
     <>
       <Typography className={classes.titleText}>Profile Information</Typography>
       <br />
-      <form>
-        <TextField
-          className={classes.settingsMargin}
-          variant="outlined"
-          color="secondary"
-          name="firstName"
-          type="text"
-          label="FirstName"
-          {...bindFirstName}
-          fullWidth
-        />
-        <TextField
-          className={classes.settingsMargin}
-          variant="outlined"
-          color="secondary"
-          name="lastName"
-          type="text"
-          label="LastName"
-          {...bindLastName}
-          fullWidth
-        />
-        <TextField
-          className={classes.settingsMargin}
-          variant="outlined"
-          color="secondary"
-          name="email"
-          type="email"
-          label="Email"
-          {...bindEmail}
-          fullWidth
-        />
-        <TextField
-          className={classes.settingsMargin}
-          variant="outlined"
-          color="secondary"
-          name="phone"
-          type="text"
-          label="Phone"
-          {...bindPhone}
-          fullWidth
-        />
-        <Button
+      <Typography>First Name: {currentUser.firstName} </Typography>
+      <Typography>Last Name: {currentUser.lastName}</Typography>
+      <Typography>Email: {currentUser.email}</Typography>
+      <Typography>Role:{currentUser.role}</Typography>
+      <div style={{ display: "flex", flexDirection: "row", alignContent: "center" }}>
+        <Typography>On Premise:<Checkbox disabled checked={currentUser.isOnPremise ? true : false} />
+        </Typography>
+        <Typography>Retail:<Checkbox disabled checked={currentUser.isRetail ? true : false} />
+        </Typography>
+      </div>
+      <Typography>Territory: {currentUser.territories.map((item) => item.name)} </Typography>
+      <Typography>State:{currentUser.state}</Typography>
+      {/* <Button
           className={classes.largeButton}
           variant="contained"
           color="secondary"
@@ -104,7 +74,6 @@ const General = () => {
         >
           SUBMIT
         </Button>
-      </form>
       <br />
       <Typography className={classes.titleText}>Account Security</Typography>
       <br />
@@ -184,7 +153,8 @@ const General = () => {
         id="twoFactor"
       >
         ENABLE TWO-FACTOR AUTHENTICATION
-      </Button>
+      </Button> 
+      */}
     </>
   );
 };
